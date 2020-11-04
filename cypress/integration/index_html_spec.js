6eba630adab7a1274e57650482a70a80ed38274d
Martin <martin.mphil@gmail.com>
2020-11-04 10:08:55 +0000

    Simple Cypress TDD ensuring html structure

--- cypress/integration/index_html_spec.js
+++ cypress/integration/index_html_spec.js
@@ -4,4 +4,12 @@ describe("home page", () => {
   it("successfully loads", () => {
     cy.visit("/");
   });
+  it("has h1 tag", () => {
+    cy.get("h1");
+  });
+  it("main element asks for answers to questions", () => {
+    cy.get("main")
+      .contains("answer", { matchCase: false })
+      .contains("question", { matchCase: false });
+  });
 });
