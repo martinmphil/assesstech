175c621fa381e00641d1b333a41f5a923d35df41
Martin <martin.mphil@gmail.com>
2020-11-13 09:23:56 +0000

    minimal id names just for testing

--- cypress/integration/assesstech_js_spec.js
+++ cypress/integration/assesstech_js_spec.js
@@ -3,12 +3,13 @@
 describe("assesstech JavaScript", () => {
   it("replaces html placeholder paragraph with scenario text", () => {
     cy.visit("/");
-    cy.get("section.scenario > p").should("have.id", "s");
-    cy.get("section.scenario > p").should("not.have.id", "s");
+    cy.get("section.scenario > p").should("have.id", "†");
+    cy.get("section.scenario > p").should("not.have.id", "†");
+    // para with id="†" from html file is replaced when js script loads.
   });
   // it("replaces html placeholder paragraph with answer choices text", () => {
   //   cy.visit("/");
-  //   cy.get("section.scenario > p").should("have.id", "c");
-  //   cy.get("section.scenario > p").should("not.have.id", "c");
+  //   cy.get("section.scenario > p").should("have.id", "‡");
+  //   cy.get("section.scenario > p").should("not.have.id", "‡");
   // });
 });
