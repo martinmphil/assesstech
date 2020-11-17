bc051a36de79f80725357dc096ca30af289e15ab
Martin <martin.mphil@gmail.com>
2020-11-17 11:44:38 +0000

    JavaScript fetches and displays answer choices

--- cypress/integration/assesstech_js_spec.js
+++ cypress/integration/assesstech_js_spec.js
@@ -7,9 +7,10 @@ describe("assesstech JavaScript", () => {
     cy.get("section.scenario > p").should("not.have.id", "†");
     // para with id="†" from html file is replaced when js script loads.
   });
-  // it("replaces html placeholder paragraph with answer choices text", () => {
-  //   cy.visit("/");
-  //   cy.get("section.scenario > p").should("have.id", "‡");
-  //   cy.get("section.scenario > p").should("not.have.id", "‡");
-  // });
+  it("replaces html placeholder paragraph with answer choices text", () => {
+    cy.visit("/");
+    cy.get("section.choices > p").should("have.id", "‡");
+    cy.get("section.choices > p").should("not.have.id", "‡");
+    // para with id="‡" from html file is replaced when js script loads.
+  });
 });
--- public/assestech.js
+++ public/assestech.js
@@ -5,8 +5,22 @@ function displayScenario(string) {
   el.innerHTML = `<p>${string}</p>`;
 }
 
+function displayChoices(array) {
+  const el = document.querySelector(".choices");
+  el.innerHTML = `
+  <p>Choices:</p>
+  <ol>
+    <li>${array[0]}</li>
+    <li>${array[1]}</li>
+    <li>${array[2]}</li>
+    <li>${array[3]}</li>
+  </ol>
+  `;
+}
+
 fetch("./phie2zubepeumahl.json")
   .then((res) => res.json())
   .then((data) => {
     displayScenario(data.scenario);
+    displayChoices(data.choices);
   });
