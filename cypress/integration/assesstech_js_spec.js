8645b32e641a2add83a94c0a798468959f8de3c7
Martin <martin.mphil@gmail.com>
2020-11-24 11:28:41 +0000

    test choices section children update after loading

--- cypress/integration/assesstech_js_spec.js
+++ cypress/integration/assesstech_js_spec.js
@@ -9,8 +9,8 @@ describe("assesstech JavaScript", () => {
   });
   it("replaces html placeholder paragraph with answer choices text", () => {
     cy.visit("/");
-    cy.get("section.choices > p").should("have.id", "‡");
-    cy.get("section.choices > p").should("not.have.id", "‡");
+    cy.get("section.choices > #‡").should("exist");
+    cy.get("section.choices > #‡").should("not.exist");
     // para with id="‡" from html file is replaced when js script loads.
   });
   it("stores answer locally", () => {
