78e94462203a91a7fe2dd1e3a467797c09c1fda4
Martin <martin.mphil@gmail.com>
2020-12-09 10:04:18 +0000

    1st html response holds Q1

--- cypress/integration/answer_choices_html_spec.js
+++ cypress/integration/answer_choices_html_spec.js
@@ -1,34 +1,48 @@
 /// <reference types="cypress" />
 
-describe("answerChoicesFrom a single element array", () => {
-  it("returns exact html", () => {
-    let array = ["choice A"];
-    let x = `<hr /><button id="a1" class="answer-choice">choice A</button><hr />`;
-    cy.visit("/").invoke("answerChoicesFrom", array).should("eq", x);
-  });
-});
-
-describe("renderChoices with four-element array", () => {
-  let array = ["choice A", "choice B", "choice C", "choice D"];
-  it("renders text with id", () => {
+describe("answer choices", () => {
+  it("holds four choices with class answer-choice", () => {
     cy.visit("/")
-      .invoke("renderChoices", array)
       .get("#a1")
+      .should("have.class", "answer-choice")
       .get("#a2")
+      .should("have.class", "answer-choice")
       .get("#a3")
+      .should("have.class", "answer-choice")
       .get("#a4")
-      .get("article.question section.choices")
-      .as("choices")
-      .contains("choice A")
-      .should("have.class", "answer-choice")
-      .get("@choices")
-      .contains("choice B")
-      .should("have.class", "answer-choice")
-      .get("@choices")
-      .contains("choice C")
-      .should("have.class", "answer-choice")
-      .get("@choices")
-      .contains("choice D")
       .should("have.class", "answer-choice");
   });
 });
+
+// describe("answerChoicesFrom a single element array", () => {
+//   it("returns exact html", () => {
+//     let array = ["choice A"];
+//     let x = `<hr /><button id="a1" class="answer-choice">choice A</button><hr />`;
+//     cy.visit("/").invoke("answerChoicesFrom", array).should("eq", x);
+//   });
+// });
+
+// describe("renderChoices with four-element array", () => {
+//   let array = ["choice A", "choice B", "choice C", "choice D"];
+//   it("renders text with id", () => {
+//     cy.visit("/")
+//       .invoke("renderChoices", array)
+//       .get("#a1")
+//       .get("#a2")
+//       .get("#a3")
+//       .get("#a4")
+//       .get("article.question section.choices")
+//       .as("choices")
+//       .contains("choice A")
+//       .should("have.class", "answer-choice")
+//       .get("@choices")
+//       .contains("choice B")
+//       .should("have.class", "answer-choice")
+//       .get("@choices")
+//       .contains("choice C")
+//       .should("have.class", "answer-choice")
+//       .get("@choices")
+//       .contains("choice D")
+//       .should("have.class", "answer-choice");
+//   });
+// });
--- public/assestech.js
+++ public/assestech.js
@@ -3,57 +3,123 @@
 main();
 
 function main() {
-  let currentQId = "phie2zubepeumahl";
-  getQuestionFor(currentQId);
+  // Add new questions to following question array
+  const questions = ["j001", "kohd3aiyaizohchi", "phie2zubepeumahl"];
+  answerListners();
+  let currentQIndex = 0;
+  let rubric = ["a4"];
+  let submission = ["a"];
+  let examPaper = {};
+
+  questions.forEach((element, index) => {
+    if (index > 0) {
+      examPaper[element] = { scenario: "", choices: ["", "", "", ""] };
+    }
+  });
+
+  //
+  //
+  // TO RMEOVE
+  //
+  //
+  console.log(questions);
+  console.log(rubric);
+  console.log(submission);
+  console.log(examPaper);
+  console.log(currentQIndex);
 }
 
-function getQuestionFor(questionID) {
-  let url = `./${questionID}.json`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      renderScenario(data.scenario);
-      renderChoices(data.choices);
-    });
-}
-
-function renderScenario(string) {
-  const el = document.querySelector(".scenario");
-  el.innerHTML = `<p>${string}</p>`;
-}
-
-function renderChoices(array) {
-  const section = document.querySelector("section.choices");
-  const html = answerChoicesFrom(array);
-  section.innerHTML = html;
-  const nodeList = section.querySelectorAll("button");
-  hearAnswersFrom(nodeList);
-}
-
-function hearAnswersFrom(nodeList) {
-  // improve via component made with Svelte
-  // see jottings repo
-  nodeList.forEach((element) => {
-    element.addEventListener("click", (event) => {
-      localStorage.setItem("question_id", event.target.id);
-      if (event.target.id === "a2") {
-        alert("​Yes, correct!");
-      } else {
-        alert("Sorry, incorrect.");
-      }
-    });
+function answerListners() {
+  // Temporary event handler while coding exam question fetching
+  //
+  // document.querySelector("#a1").addEventListener("click", (event) => {
+  //   alert(event.target.id);
+  // });
+  //
+  document.querySelector("#a1").addEventListener("click", () => {
+    alert("Sorry, incorrect.");
+  });
+  document.querySelector("#a2").addEventListener("click", () => {
+    alert("Sorry, incorrect.");
+  });
+  document.querySelector("#a3").addEventListener("click", () => {
+    alert("Sorry, incorrect.");
+  });
+  document.querySelector("#a4").addEventListener("click", () => {
+    alert("Yes, correct!");
   });
 }
 
+//
+//
+//
+// main();
+
+// function main() {
+//   let currentQId = "q001";
+//   getQuestionFor(currentQId);
+// }
+
+// function getQuestionFor(questionID) {
+//   let url = `./${questionID}.json`;
+//   fetch(url)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw Error(response.statusText);
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       renderScenario(data.scenario);
+//       renderChoices(data.choices);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
+
+// function warning() {
+//   let instructions = document.querySelector(".instructions");
+//   let warning = "Sorry we encountered a problem. Please try again later.";
+//   let html = `<p class='warning'>${warning}</p>`;
+//   instructions.innerHTML = html;
+// }
+
+// function renderScenario(string) {
+//   const el = document.querySelector(".scenario");
+//   el.innerHTML = `<p>${string}</p>`;
+// }
+
+// function renderChoices(array) {
+//   const section = document.querySelector("section.choices");
+//   const html = answerChoicesFrom(array);
+//   section.innerHTML = html;
+//   const nodeList = section.querySelectorAll("button");
+//   hearAnswersFrom(nodeList);
+// }
+
+// function hearAnswersFrom(nodeList) {
+//   nodeList.forEach((element) => {
+//     element.addEventListener("click", (event) => {
+//       localStorage.setItem("question_id", event.target.id);
+//       if (event.target.id === "a2") {
+// alert('Yes, correct!');
+//       } else {
+//         alert("Sorry, incorrect.");
+//       }
+//     });
+//   });
+// }
+
 // pure functions
 
-function answerChoicesFrom(array) {
-  let html = "<hr />";
-  html += array
-    .map((x, i) => {
-      let id = "a" + (i + 1);
-      return `<button id="${id}" class="answer-choice">${x}</button><hr />`;
-    })
-    .join("");
-  return html;
-}
+// function answerChoicesFrom(array) {
+//   let html = "<hr />";
+//   html += array
+//     .map((x, i) => {
+//       let id = "a" + (i + 1);
+//       return `<button id="${id}" class="answer-choice">${x}</button><hr />`;
+//     })
+//     .join("");
+//   return html;
+// }
