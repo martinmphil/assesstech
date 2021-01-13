ace1d385e41c5e089ef82c6288239dc41db2d8fc
Martin <martin.mphil@gmail.com>
2021-01-13 15:58:22 +0000

    add event listeners

--- public/assestech.js
+++ public/assestech.js
@@ -1,11 +1,13 @@
 import * as q4mc from "./question4mc.js";
-import * as examPaper from "./exam-paper.js";
 import * as fetching from "./fetching.js";
+import * as warning from "./warning.js";
 
 // Add new question Id's to this array.
 const questions = ["jepheil9ieb2ozie", "kohd3aiyaizohchi", "lhie2zubepeumahl"];
 
 let currentQIndex = 0;
+let submission = {};
+let examPaper = {};
 
 setUpExam(questions);
 
@@ -18,7 +20,7 @@ function setUpExam(qIdArr) {
         choices: qData.choices,
         rubric: qData.rubric,
       };
-      examPaper.questions[qId] = qObj;
+      examPaper[qId] = qObj;
       if (index === currentQIndex) {
         document.querySelector(".question").style.visibility = "visible";
         q4mc.render(qObj);
@@ -27,34 +29,37 @@ function setUpExam(qIdArr) {
   });
 }
 
-// function begin(question) {
-//   if (validateQ(question)) {
-//     render.scenario(question.scenario);
-//     render.choices(question.choices);
-//   } else {
-//     warning();
-//   }
-// }
+//
+answerListners();
 
-// function validateQ(qObj) {
-//   if (
-//     qObj.scenario.length > 0 &&
-//     qObj.choices[0].length > 0 &&
-//     qObj.choices[1].length > 0 &&
-//     qObj.choices[2].length > 0 &&
-//     qObj.choices[3].length > 0 &&
-//     qObj.rubric.length > 0
-//   ) {
-//     return true;
-//   } else {
-//     warning();
-//     // move inside fetch????
-//   }
-// }
+function answerListners() {
+  document.querySelector("#a1").addEventListener("click", (event) => {
+    handleAnswering(questions[currentQIndex], event.target.id);
+  });
+  document.querySelector("#a2").addEventListener("click", (event) => {
+    handleAnswering(questions[currentQIndex], event.target.id);
+  });
+  document.querySelector("#a3").addEventListener("click", (event) => {
+    handleAnswering(questions[currentQIndex], event.target.id);
+  });
+  document.querySelector("#a4").addEventListener("click", (event) => {
+    handleAnswering(questions[currentQIndex], event.target.id);
+  });
+}
 
-// function warning() {
-//   let instructions = document.querySelector(".instructions");
-//   let warning = "Sorry we encountered a problem. Please try again later.";
-//   let html = `<p class='warning'>${warning}</p>`;
-//   instructions.innerHTML = html;
-// }
+function handleAnswering(q, a) {
+  submission[q] = a;
+  progressQuestion();
+}
+
+function progressQuestion() {
+  currentQIndex += 1;
+  const qId = questions[currentQIndex];
+  const qObj = examPaper[qId];
+  if (currentQIndex >= questions.length) {
+    alert("END OF EXAM");
+    document.querySelector("main").innerHTML = "";
+  } else if (q4mc.validate(qObj) === true) {
+    q4mc.render(qObj);
+  } else warning.show();
+}
